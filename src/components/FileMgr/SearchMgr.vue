c4e9eee84878b589cd382f8adecf44e9b2c4de57
g122622 <guoyi122622@gmail.com>
2025-09-16 22:18:01 +0800

    chore：尝试迁移到vite

--- src/components/FileMgr/SearchMgr.vue
+++ src/components/FileMgr/SearchMgr.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, defineEmits } from "vue"
+import { computed } from "vue"
 import AdvancedTextField from "@/components/shared/AdvancedTextField.vue"
 import ResponsiveMenu from "@/components/ResponsiveLayout/ResponsiveMenu.vue"
 
---
+++ vite.config.ts
@@ -0,0 +1,83 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { VitePWA } from "vite-plugin-pwa";
+
+import process from "process";
+import os from "os";
+
+// 生成从minNum到maxNum的随机数
+const randomRange = function (minNum: number, maxNum: number): number {
+    return Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
+};
+
+const generateBuildNumber = () => {
+    const mapTable = "0123456789abcdef";
+    let res = "";
+    for (let i = 0; i < 20; i++) {
+        res += mapTable[randomRange(0, mapTable.length - 1)];
+    }
+    return res;
+};
+
+function prettyBytes(bytes: number, decimals: number = 2) {
+    if (isNaN(bytes)) return "unknown";
+    if (bytes === 0) return "0 Bytes";
+    const k = 1024;
+    const dm = decimals <= 0 ? 0 : decimals || 2;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+}
+
+const totalMem = os.totalmem();
+const freeMem = os.freemem();
+
+export default defineConfig({
+    plugins: [
+        nodePolyfills(),
+        vue(),
+        VitePWA({
+            registerType: "autoUpdate",
+            workbox: {
+                globPatterns: ["**/*.{js,css,html,ico,png,svg}"]
+            },
+            manifest: {
+                name: "Gcrypt",
+                short_name: "Gcrypt",
+                description: "跨平台文件加密一站式解决方案",
+                theme_color: "#000000",
+                icons: [
+                    {
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png"
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png"
+                    }
+                ]
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            "@": path.join(process.cwd(), "src")
+        }
+    },
+    define: {
+        COMPILE_DATE: JSON.stringify(new Date().toLocaleString()),
+        COMPILE_NUMBER: JSON.stringify(generateBuildNumber()),
+        COMPILE_PLATFORM: JSON.stringify(process.platform + " " + os.version() + " " + os.release()),
+        COMPILE_ENV: JSON.stringify(JSON.stringify(process.env, undefined, 4)),
+        COMPILE_CPU: JSON.stringify(JSON.stringify(os.cpus(), undefined, 4)),
+        COMPILE_MEM: JSON.stringify(`total: ${prettyBytes(totalMem)}, free: ${prettyBytes(freeMem)}`)
+    },
+    base: "/gcrypt/",
+    server: {
+        port: 9001
+    }
+});
